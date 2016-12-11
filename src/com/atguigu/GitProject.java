3cd76350cbc5f4e8374f762318b076e743d261e1
linghuchong8612 <linghuchong8612@126.com>
2016-12-11 20:59:50 +0800

    第一次Eclipse增加测试同步

--- src/com/atguigu/GitProject.java
+++ src/com/atguigu/GitProject.java
@@ -6,5 +6,6 @@ public class GitProject {
 		System.out.println("第一个Git项目");
 		System.out.println("第一次Git官网修改项目");
 		System.out.println("第一次测试同步");
+		System.out.println("第一次Eclipse增加测试同步");
 	}
 }
