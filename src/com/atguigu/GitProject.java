d20f9335fd0f23f1c711ac326138e523a95ce828
linghuchong8612 <linghuchong8612@126.com>
2016-12-11 20:51:33 +0800

    第一次测试同步
    第一次测试同步

--- src/com/atguigu/GitProject.java
+++ src/com/atguigu/GitProject.java
@@ -5,5 +5,6 @@ public class GitProject {
 	public static void main(String[] args) {
 		System.out.println("第一个Git项目");
 		System.out.println("第一次Git官网修改项目");
+		System.out.println("第一次测试同步");
 	}
 }
