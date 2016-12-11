5fb40159c5ed2281788e7e4826798d5ef9416991
linghuchong8612 <linghuchong8612@126.com>
2016-12-11 21:09:31 +0800

    第二次网站增加测试同步
    第二次网站增加测试同步

--- src/com/atguigu/GitProject.java
+++ src/com/atguigu/GitProject.java
@@ -7,5 +7,6 @@ public class GitProject {
 		System.out.println("第一次Git官网修改项目");
 		System.out.println("第一次测试同步");
 		System.out.println("第一次Eclipse增加测试同步");
+		System.out.println("第二次网站增加测试同步");
 	}
 }
